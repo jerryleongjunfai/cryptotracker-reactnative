3421d9303dd69c9e41318657da9237d2fec9a43b
Jerry Leong <junfai2007@gmail.com>
2021-12-10 16:19:27 +0800

    first commit

---
+++ components/ListItem.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
+
+const ListItem = ({ name, symbol, currentPrice, priceChangePercentage7d, logoUrl, onPress }) => {
+
+    const priceChangeColor = priceChangePercentage7d > 0 ? '#34C759' : '#FF3B30';
+
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <View style={styles.itemWrapper}>
+
+                {/*Left side*/}
+                <View style={styles.leftWrapper}>
+                    <Image source={{ uri: logoUrl }} style={styles.image}/>
+                    <View style={styles.titlesWrapper}>
+                        <Text style={styles.title}>{name}</Text>
+                        <Text style={styles.subTitle}>{symbol.toUpperCase()}</Text>
+                    </View>
+                </View>
+
+                {/*Right side*/}
+                <View style={styles.rightWrapper}>
+                        <Text style={styles.title}>${currentPrice.toLocaleString('en-US', { currency: 'USD' })}</Text>
+                        <Text style={[styles.subTitle, {color: priceChangeColor}]}>{priceChangePercentage7d.toFixed(2)}%</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    itemWrapper: {
+        paddingHorizontal: 16,
+        marginTop: 24,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    leftWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    image: {
+        height: 48,
+        width: 48,
+    },
+    rightWrapper: {
+        alignItems: 'flex-end',
+    },
+    titlesWrapper: {
+        marginLeft: 8,
+    },
+    title: {
+        fontSize: 18,
+    },
+    subTitle: {
+        marginTop: 4,
+        fontSize: 14,
+        color: '#A9ABB1',
+    }
+})
+
+export default ListItem
